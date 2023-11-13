f98399da12315f0e309b5a23ebf870f9f65dfc54
1Ram707 <126853321+1Ram707@users.noreply.github.com>
2023-11-13 16:49:49 +0200

    Updated App
    Updated App

--- app/src/main/java/com/example/ewing20/map/about/AboutActivity.kt
+++ app/src/main/java/com/example/ewing20/map/about/AboutActivity.kt
@@ -3,17 +3,13 @@ package com.example.ewing20.map.about
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ewing20.databinding.ActivityAboutBinding
-<<<<<<< HEAD
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-=======
->>>>>>> fd93b9e4d00e83e419284cf2e8d09eb02b516c55
 
 class AboutActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAboutBinding
-<<<<<<< HEAD
     private lateinit var webView: WebView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,12 +33,3 @@ class AboutActivity : AppCompatActivity() {
         webView.loadUrl(videoLink)
     }
 }
-=======
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityAboutBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-}
->>>>>>> fd93b9e4d00e83e419284cf2e8d09eb02b516c55
--- app/src/main/java/com/example/ewing20/map/web/WebActivity.kt
+++ app/src/main/java/com/example/ewing20/map/web/WebActivity.kt
@@ -1,19 +1,15 @@
 package com.example.ewing20.map.web
 
 import android.os.Bundle
-<<<<<<< HEAD
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
-=======
->>>>>>> fd93b9e4d00e83e419284cf2e8d09eb02b516c55
 import androidx.appcompat.app.AppCompatActivity
 import com.example.ewing20.databinding.ActivityWebBinding
 
 class WebActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityWebBinding
-<<<<<<< HEAD
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,12 +32,3 @@ class WebActivity : AppCompatActivity() {
         }*/
     }
 }
-=======
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = ActivityWebBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-}
->>>>>>> fd93b9e4d00e83e419284cf2e8d09eb02b516c55
